31c7654b1bc016e96aabf954a496481e69329bd1
Harut <harut.ghubasaryan@gmail.com>
2017-05-26 19:28:47 +0400

    modules created with structure

--- functions.js
+++ functions.js
@@ -1,7 +1,5 @@
-/**
- * Created by harutyun on 5/25/17.
- */
-var socket = new WebSocket("ws://127.0.0.1:8282");
+
+var socket = new WebSocket("ws://127.0.0.1:8787");
 socket.onopen = function () {
     alert("Connection opened")
 };
@@ -10,6 +8,17 @@ function myFunction() {
     socket.send("Hello");
 }
 socket.onmessage = function (event) {
-    alert(event.data)
+    var response = JSON.parse(event.data);
+    var command = response['command'];
+    var params = response['params'];
+    window[command](params);
 };
 
+function employee_exists(params) {
+    alert("exists "+ params['entry_id'])
+}
+
+function add_employee(params) {
+    var entry_id = params['entry_id'];
+    alert("add_employee " + entry_id)
+}
