2efd18a1c420000bc36677d8bab4d3e1c4f07fe4
Harut <harut.ghubasaryan@gmail.com>
2017-06-08 18:28:58 +0400

    file upload changes

--- functions.js
+++ functions.js
@@ -1,12 +1,11 @@
 
-var socket = new WebSocket("ws://localhost:8989");
+var socket = new WebSocket("ws://127.0.0.1:8787");
 socket.onopen = function () {
 };
 
 function myFunction() {
     socket.send(JSON.stringify({"command": "listen_reader", "params": {}}));
-	document.getElementById("addperson").style.display = "block";
-	document.getElementById("person").style.display = "none";
+
 }
 socket.onmessage = function (event) {
     var response = JSON.parse(event.data);
@@ -20,17 +19,41 @@ function employee_exists(params) {
 }
 
 function add_employee(params) {
+	document.getElementById("addperson").style.display = "block";
+	document.getElementById("person").style.display = "none";
     var entry_id = params['entry_id'];
 	document.getElementById("us_id").value = entry_id;
 }
 
+function getBase64(file) {
+   var reader = new FileReader();
+   reader.readAsDataURL(file);
+   reader.onload = function () {
+     console.log(reader.result);
+     return reader.result;
+   };
+   reader.onerror = function (error) {
+     console.log('Error: ', error);
+   };
+}
+
+
+
+
 function apply_add() {
-	
-	socket.send(
+	var files = document.getElementById("img_up");
+    var file = files.files[0];
+	var aaa = "";
+	if (files && file) {
+        var reader = new FileReader();
+        reader.onload = function(readerEvt) {
+            var binaryString = readerEvt.target.result;
+            aaa = btoa(binaryString);
+            socket.send(
 		JSON.stringify({
 			"command": "add_employee",
 			"params": {
-				"img_url": "http://windx.info/logo1.png",
+				"img_base64": aaa,
 				"entry_id": document.getElementById("us_id").value,
 				"name": document.getElementById("us_name").value,
 				"surname": document.getElementById("us_sname").value,
@@ -42,6 +65,28 @@ function apply_add() {
 			}
 		})
 	);
+        };
+        reader.readAsBinaryString(file);
+    }
+    else {
+		socket.send(
+		JSON.stringify({
+			"command": "add_employee",
+			"params": {
+				"img_base64": aaa,
+				"entry_id": document.getElementById("us_id").value,
+				"name": document.getElementById("us_name").value,
+				"surname": document.getElementById("us_sname").value,
+				"position": document.getElementById("us_pos").value,
+				"group": document.getElementById("us_group").value,
+				"start_ts": document.getElementById("us_st").value,
+				"end_ts": document.getElementById("us_et").value,
+				"comment": document.getElementById("us_com").value
+			}
+		})
+	);
+	}
+
 	document.getElementById("addperson").style.display = "none";
 	document.getElementById("person").style.display = "block";
 
